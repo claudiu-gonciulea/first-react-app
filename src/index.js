b1026f7d3f4115ea517b0295c8acb902d5c16d61
Claudiu Gonciulea <claudiu@programmers-code.org>
2024-11-13 03:43:38 +0200

    install jquery, popper and bootstrap packages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
