be6d9c3ba644d852b3fc676268d5dedd3fea3c61
Claudiu Gonciulea <claudiu@programmers-code.org>
2024-11-14 04:54:10 +0200

    add bootstrap NavBar to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
+import React from 'react';
 import {Component} from 'react';
 
 export class NavBar extends Component {
 
     render() { // react automatically calls this method when the component is rendered
-        return <h2 class="hightlight">this is a custom component</h2>;
+
+        // the solely meaning of React.Fragment tag is as a container for HTML tags,
+        // it doesn't render any HTML in the virtual DOM. 
+        // 
+
+        return <React.Fragment>
+                <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-style">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Site Name</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider" /></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    </div>
+                </div>
+                </nav>
+            </React.Fragment>;
     }
 }
