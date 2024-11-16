89bc8569117761b70e6668dc259c4c3b1c5db96a
Claudiu Gonciulea <claudiu@programmers-code.org>
2024-11-16 15:02:16 +0200

    introduce state property. read appTitle of state property

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,8 +2,16 @@ import React, {Component} from 'react';
 
 export class MainContent extends Component {
 
+    // this property contains the dynamic content that you want to render inside your component
+    // it is a javascript object. You can use nested objects inside the state
+    state = { appTitle: "Customers" };
+
     render() {
 
-        return <div>hello from main content</div>;
+        return <div>
+            
+            <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+
+        </div>;
     }
 }
